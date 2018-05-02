3b012fbbc1224090ea0ebb1ee6d41c10878dbc7e
Gustavo Segura <yo@gustavosegura.com>
2018-05-02 00:12:34 -0600

    [add]	button-matrix returns a matrix of references to the buttons 	update-buttons updates the button's label

--- GUI.rkt
+++ GUI.rkt
@@ -8,41 +8,63 @@
 ;Create a button matrix inside the parent component
 (define (button-matrix parent function matrix (j 0))
   (cond ((null? matrix)
-         #t)
+         '())
         (else
-         (button-matrix-aux (new horizontal-panel% [parent parent]
+         (cons (button-matrix-aux (new horizontal-panel% [parent parent]
                                      [alignment '(center center)])
                             function
                             (car matrix)  j 0)
-         (button-matrix parent function  (cdr matrix) (+ j 1) )
+               (button-matrix parent function  (cdr matrix) (+ j 1) )
+               )
+         
          )
     )
   )
 
 (define (button-matrix-aux parent function row  i j )
   (cond ((null? row)
-         #t)
+         '())
         (else
-         (new button% [parent parent]
+         (cons (new button% [parent parent]
              [label  (~a (car row))]
              ; Callback procedure for a button click:
              [callback (lambda (button event)
                          ;(send msg set-label (string-append   "Button click "  (~a i) " "  (~a j) ))
                          (function (list i j))
                          )]
-             )
-         (button-matrix-aux parent  function  (cdr row) i (+ j 1))
+             ) (button-matrix-aux parent  function  (cdr row) i (+ j 1)) )
+         
          )
     )
   )
+
+
+(define (update-buttons BM M)
+  (for-each
+   (lambda (BR R) (update-buttons-aux BR R)) BM M)
+  )
+
+(define (update-buttons-aux BR R)
+  (for-each
+   (lambda (B E) (send B set-label (~a E))) BR R )
+  )
+
 (define t3 '((O O O X X -)
              (O - O X - X)
              (O X O O O X)
              (X X X O - -)
              (- O - - - X)
              (O X - O X X)))
+(define t4 '((X X X X X X)
+             (X X X X X X)
+             (X X X X X X)
+             (X X X O - -)
+             (- O - - - X)
+             (O X - O X X)))
 
 
-(button-matrix frame pretty-print t3 )
+(define BM (button-matrix frame pretty-print t3 ))
+;BM
+(update-buttons BM t4)
 ; Show the frame by calling its show method
 (send frame show #t)
